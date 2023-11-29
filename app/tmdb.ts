c96d078905540b29977e4fa82f807c25a8c147c7
Jon Stuebe <jstuebe@gmail.com>
2023-11-29 09:53:43 -0700

    feat: removed axios

--- app/tmdb.ts
+++ app/tmdb.ts
@@ -1,46 +1,45 @@
-import axios from "axios";
-
 import env from "./env";
 
 import type { MovieData } from "./types";
 
-const api = axios.create({
-  baseURL: "https://api.themoviedb.org/3",
-  headers: {
-    Accept: "application/json",
-    Authorization: `Bearer ${env.TMDB_API_READ_ACCESS_TOKEN}`,
-  },
-});
-
 export async function getMovie({ title, year }: MovieData) {
-  const { results } = await api
-    .get<{
-      page: number;
-      results: {
-        adult: boolean;
-        backdrop_path: string;
-        genre_ids: number[];
-        id: number;
-        original_language: string;
-        original_title: string;
-        overview: string;
-        popularity: number;
-        poster_path: string;
-        release_date: string;
-        title: string;
-        video: boolean;
-        vote_average: number;
-        vote_count: number;
-      }[];
-      total_pages: number;
-      total_results: number;
-    }>("/search/movie", {
-      params: {
+  const data = await fetch(
+    "https://api.themoviedb.org/3/search/movie?" +
+      new URLSearchParams({
         query: title,
         year,
+      }).toString(),
+    {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${env.TMDB_API_READ_ACCESS_TOKEN}`,
       },
-    })
-    .then((r) => r.data);
+    }
+  ).then(
+    (r) =>
+      r.json() as Promise<{
+        page: number;
+        results: {
+          adult: boolean;
+          backdrop_path: string;
+          genre_ids: number[];
+          id: number;
+          original_language: string;
+          original_title: string;
+          overview: string;
+          popularity: number;
+          poster_path: string;
+          release_date: string;
+          title: string;
+          video: boolean;
+          vote_average: number;
+          vote_count: number;
+        }[];
+        total_pages: number;
+        total_results: number;
+      }>
+  );
 
-  return results[0];
+  return data.results[0];
 }
